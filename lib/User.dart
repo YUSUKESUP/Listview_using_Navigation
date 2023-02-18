cd4b60eb386131a45deee08760939cc9f042dcfc
YUUSKE <yusuke.1173ab@gmail.com>
2023-02-18 16:46:56 +0900

    first commit

---
+++ lib/User.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:listview_with_navigator/main.dart';
+
+class UserPage extends StatelessWidget {
+  final User user;
+  const UserPage({Key? key,
+    required this.user
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(user.username),
+      ),
+      body: Center(
+        child: Text(user.email,
+          style: TextStyle(
+            fontSize: 20
+          ),
+        ),
+      ),
+    );
+  }
+}
